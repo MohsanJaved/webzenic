fb4a75d57c17b16a8924fb283d81d3c3d2f78ae6
Mohsan Javed <khanmohsan166@gmail.com>
2025-08-15 12:17:00 +0500

    logo+favicon

--- src/Components/Header/Header2.jsx
+++ src/Components/Header/Header2.jsx
@@ -41,7 +41,7 @@ export default function Header2({ variant }) {
           <div className="cs_main_header_in">
             <div className="cs_main_header_left">
             <Link className="cs_site_branding" to="/">
-                <img src="/assets/img/logo/black-logo.svg" alt="Logo" />
+                <img className='h-logo' src="/assets/img/logo/black-logo.png" alt="Logo" />
               </Link>
               </div>
               <div className="cs_main_header_center">
