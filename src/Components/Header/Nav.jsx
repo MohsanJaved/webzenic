b9d6cdf424030be8b6e7ec264e80ebe8bcf98ba8
Mohsan Javed <khanmohsan166@gmail.com>
2025-08-17 11:14:08 +0500

    contac us

--- src/Components/Header/Nav.jsx
+++ src/Components/Header/Nav.jsx
@@ -31,43 +31,7 @@ export default function Nav({ setMobileToggle }) {
           </ul>
         </DropDown>
       </li> */}
-      <li className="menu-item-has-children">
-        <Link to="#">Pages</Link>
-        <DropDown>
-          <ul>
-            <li>
-              <Link to="/about" onClick={() => setMobileToggle(false)}>
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link to="/team" onClick={() => setMobileToggle(false)}>
-               Our Team
-              </Link>
-            </li>            
-            <li>
-              <Link to="/team/team-details" onClick={() => setMobileToggle(false)}>
-                Team Details
-              </Link>
-            </li>             
-            <li>
-              <Link to="/pricing" onClick={() => setMobileToggle(false)}>
-              Pricing
-              </Link>
-            </li> 
-            <li>
-              <Link to="/faq" onClick={() => setMobileToggle(false)}>
-              Faq
-              </Link>
-            </li> 
-            <li>
-              <Link to="/contact" onClick={() => setMobileToggle(false)}>
-              Contact
-              </Link>
-            </li>                         
-          </ul>
-        </DropDown>
-      </li>  
+    
 
       <li className="menu-item-has-children">
         <Link to="/service" onClick={() => setMobileToggle(false)}>
@@ -141,6 +105,30 @@ export default function Nav({ setMobileToggle }) {
           Contact
         </Link>
       </li> */}
+        <li className="menu-item-has-children">
+        <Link to="#">Pages</Link>
+        <DropDown>
+          <ul>
+            <li>
+              <Link to="/about" onClick={() => setMobileToggle(false)}>
+                About Us
+              </Link>
+            </li>
+           
+           
+            <li>
+              <Link to="/faq" onClick={() => setMobileToggle(false)}>
+              Faq
+              </Link>
+            </li> 
+            <li>
+              <Link to="/contact" onClick={() => setMobileToggle(false)}>
+              Contact
+              </Link>
+            </li>                         
+          </ul>
+        </DropDown>
+      </li>  
     </ul>
   );
 }
