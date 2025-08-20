7ff79acd8e044a290cf73f7acd356c4192724ceb
Mohsan Javed <148203693+MohsanJaved@users.noreply.github.com>
2025-08-20 21:19:25 +0500

    Update Main.jsx

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header2 from '../Components/Header/Header2';
-import Footer1 from '../Components/Footer/Footer1';
+import Footer2 from '../Components/Footer/Footer2';
 
 const Main = () => {
     return (
         <div className='main-page-area'>
             <Header2></Header2>
             <Outlet></Outlet>
-           <Footer1></Footer1>
+           <Footer2></Footer2>
         </div>
     );
 };
