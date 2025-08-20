e7966375fa86e86d2a73523609b68067bc574423
Mohsan Javed <148203693+MohsanJaved@users.noreply.github.com>
2025-08-20 20:38:28 +0500

    Update Services3.jsx

--- src/Components/Services/Services3.jsx
+++ src/Components/Services/Services3.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Services3 = () => {
 
     const chooseContent = [
-        {img:'/assets/img/service/icon-1.png', title:'Digital Marketing', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
-        {img:'/assets/img/service/icon-2.png', title:'SEO Analytics', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
-        {img:'/assets/img/service/icon-3.png', title:'Social Marketing', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
-        {img:'/assets/img/service/icon-4.png', title:'Email Marketing', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
-        {img:'/assets/img/service/icon-5.png', title:'Social Media Marketing', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
+        {img:'/assets/img/service/icon-1.png', title:'Website design and development', content:'Design tailored websites offering unique functionality to match business needs.'},
+        {img:'/assets/img/service/icon-2.png', title:' Custom website development', content:'Design bespoke websites offering unique functionality to match business needs.'},
+        {img:'/assets/img/service/icon-3.png', title:' E-commerce website development', content:'Build powerful online stores that drive sales, conversions, and customer satisfaction.'},
+        {img:'/assets/img/service/icon-4.png', title:' Shopify website development', content:'Create scalable Shopify stores with seamless design and optimized performance.'},
+        {img:'/assets/img/service/icon-5.png', title:' WordPress website development', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
         {img:'/assets/img/service/icon-6.png', title:'Better audiences', content:'Lorem dolor amet consectetur read adipiscing any more elit.'},
       ]; 
 
